01785dbfc5b64997f237b7532c2c856323761ece
phancuong1234 <pvtho1111@gmail.com>
2020-04-09 10:40:56 +0700

    update

--- src/app/normal/normal.component.ts
+++ src/app/normal/normal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit, Injectable, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppService } from '../app.service';
 import { CookieService } from "ngx-cookie-service";
@@ -10,7 +10,7 @@ import { CookieService } from "ngx-cookie-service";
   templateUrl: './normal.component.html',
   styleUrls: ['./normal.component.css']
 })
-export class NormalComponent implements OnInit {
+export class NormalComponent implements OnInit, OnDestroy {
   constructor(private router: Router, private appService: AppService, private cookie: CookieService) { }
 
   ngOnInit() {
@@ -48,36 +48,14 @@ export class NormalComponent implements OnInit {
 
   async logout() {
     localStorage.setItem('isLogin', 'false');
-    window['zE']('webWidget', 'logout');
-    window.location.reload(false);
-    // window['zE']('webWidget:on', 'chat:status', function(status) {
-    //   console.log('This chat session is now', status);
-    // });
-    // window['zE']('webWidget:on', 'chat:connected', function() {
-    //   console.log('successfully connected to Zendesk Chat!');
-    // });
+    await window['zE']('webWidget', 'logout');
   }
 
   async login() {
     await this.cookie.set('token', '123');
-    // var js = document.getElementById("ze-snippet");
-    // if(js !== null) {
-    //     document.body.removeChild(js);
-    //     console.info("---------- Script refreshed ----------");
-    // }
-    // // console.log(js.attributes);
-
-    // // Create new script element and load a script into it
-    // var newElementScript = document.createElement("script");
-    // newElementScript.setAttribute("id", "ze-snippet");
-    // newElementScript.setAttribute("src", js.attributes[1]['value'].split("?time=")[0] + "?time=" + Math.floor(Math.random()* 10000000));
-    // document.body.append(newElementScript);
-    // console.log(1);
-    await this.resetWidget();
     await localStorage.setItem('isLogin', 'true');
+    await this.reloadf();
     await this.appService.getToken();
-    console.log(2)
-    // window.location.reload(true);
   }
   resetWidget() {
     var js = document.getElementById("ze-snippet");
@@ -94,30 +72,35 @@ export class NormalComponent implements OnInit {
     document.body.appendChild(newElementScript);
   }
   replace() {
-    var js = document.getElementById("ze-snippet");
-    if(js !== null) {
-        document.body.removeChild(js);
-        console.info("---------- Script refreshed ----------");
-    }
-    console.log(js.attributes)
-    var newElementScript = document.createElement("script");
-    newElementScript.setAttribute("id", "ze-snippet");
-    newElementScript["src"] = "assets/my.js";
-    document.body.appendChild(newElementScript);
+    // var js = document.getElementById("ze-snippet");
+    // if(js !== null) {
+    //     document.body.removeChild(js);
+    //     console.info("---------- Script refreshed ----------");
+    // }
+    // console.log(js.attributes)
+    // var newElementScript = document.createElement("script");
+    // newElementScript.setAttribute("id", "ze-snippet");
+    // newElementScript["src"] = "assets/my.js";
+    // document.body.appendChild(newElementScript);
   }
   goToOtc() {
     if(window['zE']('webWidget:get', 'chat:isChatting')) {
-      window['zE']('webWidget', 'chat:end');
       window['zE']('webWidget:on', 'chat:end', function() {
-        window.location.reload(false);
-        window.location.href = "/otc";
+        alert(1)
+        console.log(1)
+        setTimeout(()=>{
+          this.router.navigateByUrl("/otc");
+        },0)
+        console.log(2)
+        console.log(this.router)
+        
       });
+      window['zE']('webWidget', 'chat:end');
     } else {
-      window.location.reload(false);
-      window.location.href = "/otc";
+      alert(20)
+      this.router.navigateByUrl("/otc");
     }
     
-    // this.router.navigateByUrl('otc');
   }
   reload() {
     window['zE']('webWidget', 'chat:end');
@@ -129,10 +112,11 @@ export class NormalComponent implements OnInit {
   show() {
     window['zE']('webWidget', 'show');
   }
-  ngDestroy() {
-
+   ngOnDestroy() {
+        window['zE']('webWidget', 'hide');
   }
   reloadf() {
+    this.resetWidget();
     const elementToRemove = document.getElementById('ze-snippet');
     const elementsWhichParentsToRemove = document.querySelectorAll('.zEWidget-launcher, .zEWidget-webWidget, [data-product="web_widget"]');
     if (!!elementToRemove) {
@@ -146,52 +130,29 @@ export class NormalComponent implements OnInit {
     }
     window['zEmbed'] = null;
     window['zE'] = null;
-    window['zEmbed'] || function (e, t) { 
-    var n, o, d, i, s, a = [], r = document.createElement("iframe"); 
-    window['zEmbed'] = function () { 
-      a.push(arguments) 
-    }, 
-    window['zE'] = window['zE'] || window['zE'],
-    r.src = "javascript:false",
-    r.title = "",
-    r['role'] = "presentation",
-    (r['frameElement'] || r).id="zendesk-include-script-iframe",
-    (r['frameElement'] || r).style.cssText = "display: none",
-    d = document.getElementsByTagName("script"), d = d[d.length - 1],
-    d.parentNode.insertBefore(r, d), i = r.contentWindow, s = i.document; 
-    try {
-      o = s 
-    } catch (e) {
-       n = document.domain,
-      r.src = 'javascript:var d=document.open();d.domain="' + n + '";void(0);',
-       o = s } o.open()._l = function () {
-        var e = this.createElement("script");
-        n && (this.domain = n),
-        e.id = "js-iframe-async",
-        e.src = "https://assets.zendesk.com/embeddable_framework/main.js",
-        this.t = +new Date, this.zendeskHost = "cryptonomics.zendesk.com",
-        this.zEQueue = a, this.body.appendChild(e) },
-        o.write('<body onload="document._l();">'),
-        o.close() 
-      }();
-      // zE(function () {            
-      //     if (activate) {
-      //         zE.setLocale(getLanguage());
-      //         zE.identify({
-      //             name: `${store.state.appContext.initialConfig.UserData.FirstName} ${store.state.appContext.initialConfig.UserData.LastName}`,
-      //             email: store.state.appContext.initialConfig.UserData.Email,
-      //         });
-      //     } else {
-      //         zE.hide();
-      //     }
-      // });
-    //   window['zE'](function () {            
-    //     if (true) {
-    //       // window['zE'].setLocale(getLanguage());
-          
-    //     } else {
-    //       window['zE'].hide();
-    //     }
-    // });
+    window['zEmbed'] || function(e,t) {
+      var n,o,d,i,s,a=[],r=document.createElement("iframe");
+      window['zEmbed'] =function() {
+        a.push(arguments)},
+        window['zE'] = window['zE'] || window['zEmbed'],
+        r.src="javascript:false",
+        r.title="",
+        r['role']="presentation",
+        (r['frameElement']||r).style.cssText="display: none",
+        d=document.getElementsByTagName("script"),
+        d=d[d.length-1],d.parentNode.insertBefore(r,d),
+        i=r.contentWindow,s=i.document;try{o=s}catch(e){
+          n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',
+          o=s
+        }
+        o.open()._l=function(){
+          var e=this.createElement("script");
+          n&&(this.domain=n),e.id="js-iframe-async",
+          e.src="https://assets.zendesk.com/embeddable_framework/main.js",
+          this.t=+new Date,this.zendeskHost="cryptonomics.zendesk.com",
+          this.zEQueue=a,this.body.appendChild(e)},
+          o.write('<body onload="document._l();">'),
+          o.close()
+        }();
   }
 }
--- src/app/otc/otc.component.ts
+++ src/app/otc/otc.component.ts
@@ -13,15 +13,18 @@ import { CookieService } from "ngx-cookie-service";
 })
 export class OtcComponent implements OnInit {
 
-  constructor(private route: Router, private appService: AppService, private cookie: CookieService) { }
+  constructor(private router: Router, private appService: AppService, private cookie: CookieService) { }
 
   ngOnInit() {
+    // window['zE']('webWidget:on', 'chat:end', function() {
+      this.reloadf();
+    // });
+    
     // if(!window['zE']('webWidget:get', 'chat:isChatting')) {
     //   window.location.reload(false);
     // }
     // console.log(this.cookie.get('token'));
     window['zE']('webWidget:on', 'chat:connected', function() {
-      console.log('hi')
       window['zE']('webWidget', 'show');
     });
     this.appService.getToken();
@@ -43,17 +46,56 @@ export class OtcComponent implements OnInit {
     // });
   }
   back() {
+    this.router.navigateByUrl("/");
+  }
+  reloadf() {
+    const elementToRemove = document.getElementById('ze-snippet');
+    const elementsWhichParentsToRemove = document.querySelectorAll('.zEWidget-launcher, .zEWidget-webWidget, [data-product="web_widget"]');
+    if (!!elementToRemove) {
+      elementToRemove.remove();
+    }
+    if (!!elementsWhichParentsToRemove) {
+      Array.from(elementsWhichParentsToRemove).forEach((element) => {
+        console.log(element)
+        element.remove()
+      });
+    }
+    window['zEmbed'] = null;
+    window['zE'] = null;
+    window['zEmbed'] || function(e,t) {
+      var n,o,d,i,s,a=[],r=document.createElement("iframe");
+      window['zEmbed'] =function() {
+        a.push(arguments)},
+        window['zE'] = window['zE'] || window['zEmbed'],
+        r.src="javascript:false",
+        r.title="",
+        r['role']="presentation",
+        (r['frameElement']||r).style.cssText="display: none",
+        d=document.getElementsByTagName("script"),
+        d=d[d.length-1],d.parentNode.insertBefore(r,d),
+        i=r.contentWindow,s=i.document;try{o=s}catch(e){
+          n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',
+          o=s
+        }
+        o.open()._l=function(){
+          var e=this.createElement("script");
+          n&&(this.domain=n),e.id="js-iframe-async",
+          e.src="https://assets.zendesk.com/embeddable_framework/main.js",
+          this.t=+new Date,this.zendeskHost="cryptonomics.zendesk.com",
+          this.zEQueue=a,this.body.appendChild(e)},
+          o.write('<body onload="document._l();">'),
+          o.close()
+        }();
+
+  }
+  ngDestroy() {
     if(window['zE']('webWidget:get', 'chat:isChatting')) {
       window['zE']('webWidget', 'chat:end');
       window['zE']('webWidget:on', 'chat:end', function() {
-        window.location.reload(false);
-        window.location.href = "/";
+        window['zE']('webWidget', 'hide');
       });
     } else {
-      window.location.reload(false);
-      window.location.href = "/";
+      window['zE']('webWidget', 'hide');
     }
-    // this.route.navigateByUrl('/');
   }
-  ngDestroy() {}
 }
